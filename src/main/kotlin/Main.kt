0e45a98b9535bfdefca6ce32adba1467e6809a29
Josh Crotty <9255594+Zaradin@users.noreply.github.com>
2023-02-02 15:40:21 +0000

    Added person class & created objects from Person

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package ie.setu.classes
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import Person
+
+fun main(args: Array<String>) {
+
+    val person1 = Person("Mark", "Roche")
+    val person2 = person1.copy(firstName = "Clare")
+    val person3 = person1.copy()
+
+    println("person1 hashcode = ${person1.hashCode()}")
+    println("person2 hashcode = ${person2.hashCode()}")
+    println("person3 hashcode = ${person3.hashCode()}")
+
+    if (person1.equals(person2))
+        println("person1 is equal to person2.")
+    else
+        println("person1 is not equal to person2.")
+
+    if (person1.equals(person3))
+        println("person1 is equal to person3.")
+    else
+        println("person1 is not equal to person3.")
+
+
 }
